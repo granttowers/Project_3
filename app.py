aad740519ffef3343e5f37d947ed8890d2aab7c4
Grant Towers <grant_towers@bigpond.com>
2022-08-22 19:24:53 +0800

    Added Procfile and requirements files

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
-import numpy as np
-import psycopg2
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import inspect, create_engine, func
-
-from flask import Flask, jsonify, render_template
-
-from DB_keys import db_key
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
 #################################################
 # Database Setup
 #################################################
 
 # Create the engine to connect to local server
-engine = (create_engine(f'postgresql://postgres:' +
-          db_key+f'@localhost:5432/Project_2'))
+# engine = (create_engine(f'postgresql://postgres:' +
+#           db_key+f'@localhost:5432/Project_2'))
 
-# reflect an existing database into a new model
-Base = automap_base()
+# # reflect an existing database into a new model
+# Base = automap_base()
 
-# reflect the tables
-Base.prepare(engine, reflect=True)
-engine.table_names()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+# engine.table_names()
 
-# Save references to each table
-incident_details = Base.classes.incident_details
-injured_person_details = Base.classes.injured_person_details
-injury_details = Base.classes.injury_details
-company_details = Base.classes.company_details
+# # Save references to each table
+# incident_details = Base.classes.incident_details
+# injured_person_details = Base.classes.injured_person_details
+# injury_details = Base.classes.injury_details
+# company_details = Base.classes.company_details
 
-# #################################################
-# # Flask Setup
-# #################################################
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
-
 
 # #################################################
 # # Flask Routes
@@ -52,101 +48,101 @@ def welcome():
     #     f"/api/v1.0/company_details"
     # )
 
-@app.route("/api/v1.0/incident_details")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/incident_details")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of all incident details"""
+#     """Return a list of all incident details"""
 
-    # Query all incident details
-    results = session.query(incident_details.mine_id, incident_details.incident_activity, incident_details.incident_category,
-                            incident_details.incident_day, incident_details.incident_month, incident_details.incident_type, incident_details.incident_year).all()
+#     # Query all incident details
+#     results = session.query(incident_details.mine_id, incident_details.incident_activity, incident_details.incident_category,
+#                             incident_details.incident_day, incident_details.incident_month, incident_details.incident_type, incident_details.incident_year).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all incidents
-    incident_test = []
+#     # Create a dictionary from the row data and append to a list of all incidents
+#     incident_test = []
 
-    for mine_id, incident_activity, incident_category, incident_day, incident_month, incident_type, incident_year in results:
+#     for mine_id, incident_activity, incident_category, incident_day, incident_month, incident_type, incident_year in results:
 
-        incident_test_dict = {}
+#         incident_test_dict = {}
 
-        incident_test_dict["mine_id"] = mine_id
-        incident_test_dict["incident_activity"] = incident_activity
-        incident_test_dict["incident_category"] = incident_category
-        incident_test_dict["incident_day"] = incident_day
-        incident_test_dict["incident_month"] = incident_month
-        incident_test_dict["incident_type"] = incident_type
-        incident_test_dict["incident_year"] = incident_year
+#         incident_test_dict["mine_id"] = mine_id
+#         incident_test_dict["incident_activity"] = incident_activity
+#         incident_test_dict["incident_category"] = incident_category
+#         incident_test_dict["incident_day"] = incident_day
+#         incident_test_dict["incident_month"] = incident_month
+#         incident_test_dict["incident_type"] = incident_type
+#         incident_test_dict["incident_year"] = incident_year
 
-        incident_test.append(incident_test_dict)
+#         incident_test.append(incident_test_dict)
 
-    return jsonify(incident_test)
-
-
-@app.route("/api/v1.0/injury_details")
-def names2():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all injury details"""
-
-    # Query all injury details
-    results = session.query(injury_details.mine_id, injury_details.injury_body_part, injury_details.injury_nature,
-                            injury_details.injury_classification, injury_details.injury_days_lost).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all injuries
-    injury_details_test = []
-
-    for mine_id, injury_body_part, injury_nature, injury_classification, injury_days_lost in results:
-
-        injury_details_dict = {}
-
-        injury_details_dict["mine_id"] = mine_id
-        injury_details_dict["injury_body_part"] = injury_body_part
-        injury_details_dict["injury_nature"] = injury_nature
-        injury_details_dict["injury_classification"] = injury_classification
-        injury_details_dict["injury_days_lost"] = injury_days_lost
-
-        injury_details_test.append(injury_details_dict)
-
-    return jsonify(injury_details_test)
+#     return jsonify(incident_test)
 
 
-@app.route("/api/v1.0/company_details")
-def names3():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/injury_details")
+# def names2():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of all company details"""
+#     """Return a list of all injury details"""
 
-    # Query all company details
-    results = session.query(company_details.mine_id, company_details.company_mine_type, company_details.company_name, company_details.company_address,
-                            company_details.company_city, company_details.company_injury_count, company_details.company_underground_surface).all()
+#     # Query all injury details
+#     results = session.query(injury_details.mine_id, injury_details.injury_body_part, injury_details.injury_nature,
+#                             injury_details.injury_classification, injury_details.injury_days_lost).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all companies
-    company_details_test = []
+#     # Create a dictionary from the row data and append to a list of all injuries
+#     injury_details_test = []
 
-    for mine_id, company_mine_type, company_name, company_address, company_city, company_injury_count, company_underground_surface in results:
+#     for mine_id, injury_body_part, injury_nature, injury_classification, injury_days_lost in results:
 
-        company_details_dict = {}
+#         injury_details_dict = {}
 
-        company_details_dict["mine_id"] = mine_id
-        company_details_dict["company_mine_type"] = company_mine_type
-        company_details_dict["company_name"] = company_name
-        company_details_dict["company_address"] = company_address
-        company_details_dict["company_city"] = company_city
-        company_details_dict["company_injury_count"] = company_injury_count
-        company_details_dict["company_underground_surface"] = company_underground_surface
+#         injury_details_dict["mine_id"] = mine_id
+#         injury_details_dict["injury_body_part"] = injury_body_part
+#         injury_details_dict["injury_nature"] = injury_nature
+#         injury_details_dict["injury_classification"] = injury_classification
+#         injury_details_dict["injury_days_lost"] = injury_days_lost
 
-        company_details_test.append(company_details_dict)
+#         injury_details_test.append(injury_details_dict)
 
-    return jsonify(company_details_test)
+#     return jsonify(injury_details_test)
+
+
+# @app.route("/api/v1.0/company_details")
+# def names3():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all company details"""
+
+#     # Query all company details
+#     results = session.query(company_details.mine_id, company_details.company_mine_type, company_details.company_name, company_details.company_address,
+#                             company_details.company_city, company_details.company_injury_count, company_details.company_underground_surface).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all companies
+#     company_details_test = []
+
+#     for mine_id, company_mine_type, company_name, company_address, company_city, company_injury_count, company_underground_surface in results:
+
+#         company_details_dict = {}
+
+#         company_details_dict["mine_id"] = mine_id
+#         company_details_dict["company_mine_type"] = company_mine_type
+#         company_details_dict["company_name"] = company_name
+#         company_details_dict["company_address"] = company_address
+#         company_details_dict["company_city"] = company_city
+#         company_details_dict["company_injury_count"] = company_injury_count
+#         company_details_dict["company_underground_surface"] = company_underground_surface
+
+#         company_details_test.append(company_details_dict)
+
+#     return jsonify(company_details_test)
 
 
 if __name__ == '__main__':
