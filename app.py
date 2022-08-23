a1ffae53b8cd57a7a9fcb653847651aad51456bf
Grant Towers <grant_towers@bigpond.com>
2022-08-24 00:08:07 +0800

    Updates to pages

--- app.py
+++ app.py
@@ -399,14 +399,14 @@ def answers(sex, age, occupation, operational_process):
     NOI_model = 'NOI_rf.pickle'
     NOI_loaded_model = pickle.load(open(NOI_model, 'rb'))
 
-    # IBP_model = 'IBP_rf.pickle'
-    # IBP_loaded_model = pickle.load(open(IBP_model, 'rb'))
+    IBP_model = 'IBP_rf.pickle'
+    IBP_loaded_model = pickle.load(open(IBP_model, 'rb'))
 
-    # SOI_model = 'SOI_rf.pickle'
-    # SOI_loaded_model = pickle.load(open(SOI_model, 'rb'))
+    SOI_model = 'SOI_rf.pickle'
+    SOI_loaded_model = pickle.load(open(SOI_model, 'rb'))
 
-    # ACT_model = 'ACT_rf.pickle'
-    # ACT_loaded_model = pickle.load(open(ACT_model, 'rb'))
+    ACT_model = 'ACT_rf.pickle'
+    ACT_loaded_model = pickle.load(open(ACT_model, 'rb'))
 
     # Full response table to collate the response values
     response = [Sex_Male, Sex_Female, Age_Under_20, Age_20_29, Age_30_39, Age_40_49, Age_50_59, Age_60_69, Age_70_79, Age_80_89, Occupation_Apprentice_or_Trainee_Roles, Occupation_Driller_and_Support_Roles, Occupation_Explosives_Roles, Occupation_Fixed_Plant_Operator_and_Support_Roles, Occupation_Labourer_Roles, Occupation_Maritime_Roles, Occupation_Mine_Supervisory_and_Management_Roles, Occupation_Mobile_Plant_Operator_Roles, Occupation_Other_Role, Occupation_Technical_Services_Roles, Occupation_Trades_Roles, Occupation_Underground_Miner_and_Support_Roles, Operational_Process_Crushing_or_Processing_Facility_Incl_Associated_Workshops_and_Yards, Operational_Process_Culm_Banks_Activities_Coal_Mining_Only, Operational_Process_Dredging_Activities, Operational_Process_Non_Mine_Workshops_and_Yards, Operational_Process_Office_Located_on_Mine, Operational_Process_Other_Surface_Facility, Operational_Process_Surface_Mining_Operations, Operational_Process_Underground_Mine_Surface_Workshops_and_Yards, Operational_Process_Underground_Mining_Operations]
